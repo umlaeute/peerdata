6339e18fa6ffbb70d926574a37c3c06d41056103
zmoelnig <zmoelnig@5c11c4bb-bd2d-0410-856d-cfe5d47ff667>
2008-07-31 14:39:45 +0000

    updated
    git-svn-id: https://svn.umlaeute.mur.at/svnroot/zmoelnig/projects/peerdata@310 5c11c4bb-bd2d-0410-856d-cfe5d47ff667

--- prepare.sh
+++ prepare.sh
@@ -4,12 +4,8 @@ TARGETDIR=/tmp/sandbox
 SOURCEDIR=.
 
 mkdir -p ${TARGETDIR}
-cp ${SOURCEDIR}/sandbox/patch.pd.template ${TARGETDIR}/patch.pd
-
-cp ${SOURCEDIR}/action/*.pd ${TARGETDIR}/
-
+cp ${SOURCEDIR}/sandbox/* ${TARGETDIR}/
 cp ${SOURCEDIR}/x86_64/* ${TARGETDIR}
-cp ${SOURCEDIR}/misc/* ${TARGETDIR}
 
 
 
